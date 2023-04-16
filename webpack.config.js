490aaedfbe691cb8c494432c7de171a27d781a57
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-04-16 00:39:28 -0300

    style: apply eslint fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -9,46 +12,46 @@ module.exports = {
     filename: 'bundle-[hash].js',
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx']
+    extensions: [ '.ts', '.tsx', '.js', 'jsx' ]
   },
   module: {
     rules: [
-    {
-      test: /\.(ts|tsx)$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: [
-            '@babel/preset-env',
-            '@babel/preset-react',
-            '@babel/preset-typescript'
-          ]
-        }
-      }
-    },
-    {
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'style-loader'
-        },
-        {
-          loader: 'css-loader',
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
           options: {
-            modules: true
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
           }
-        },
-        {
-          loader: 'sass-loader'
         }
-      ]
-    }
-  ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
   ]
-}
+};
