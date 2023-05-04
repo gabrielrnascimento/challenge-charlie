6c7fb317afd9976ce1550af1d0e2ad56b752c4f1
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-03 21:33:39 -0300

    test: move SUT creation to a factory method

--- src/infra/http/axios/axios-http-client.spec.ts
+++ src/infra/http/axios/axios-http-client.spec.ts
@@ -1,3 +1,5 @@
+import type axios from 'axios';
+
 import { mockHttpRequest } from '@/data/test';
 import { mockAxios } from '@/infra/test/mock-axios';
 
@@ -5,12 +7,25 @@ import { AxiosHttpClient } from './axios-http-client';
 
 jest.mock('axios');
 
+type SutTypes = {
+  sut: AxiosHttpClient,
+  mockedAxios: jest.Mocked<typeof axios>
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  const mockedAxios = mockAxios();
+  return {
+    sut,
+    mockedAxios
+  };
+};
+
 describe('AxiosHttpClient', () => {
   test('should call axios with correct params', async () => {
+    const { sut, mockedAxios } = makeSut();
     const request = mockHttpRequest();
-    const axiosHttpClient = new AxiosHttpClient();
-    const mockedAxios = mockAxios();
-    await axiosHttpClient.request(request);
+    await sut.request(request);
     expect(mockedAxios.request).toHaveBeenCalledWith({
       url: request.url,
       method: request.method,
