d3f98289857035e806bccf67d565d88ae26cd039
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-03 23:11:04 -0300

    feat: ensure AxiosHttpClient returns correct error

--- src/infra/http/axios/axios-http-client.ts
+++ src/infra/http/axios/axios-http-client.ts
@@ -1,11 +1,27 @@
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
 
-import { type HttpRequest } from '@/data/protocols/http';
+import { type HttpResponse, type HttpClient, type HttpRequest } from '@/data/protocols/http';
 
-export class AxiosHttpClient {
-  async request (params: HttpRequest): Promise<any> {
-    const response = await axios.request(params);
-    return Promise.resolve(response);
+export class AxiosHttpClient implements HttpClient<HttpResponse> {
+  async request (params: HttpRequest): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+    try {
+      axiosResponse = await axios.request(params);
+    } catch (error: any) {
+      if (error.response) {
+        axiosResponse = error.response;
+      }
+      else if (error.request) {
+        axiosResponse = error.request;
+      }
+      else {
+        axiosResponse = error.message;
+      }
+    }
+    return {
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
+    };
   }
 }
 
