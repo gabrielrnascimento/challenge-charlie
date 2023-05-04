08a9f03024b007f5a29557330b8cc6167b3fdefa
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-03 21:50:11 -0300

    test: move request variable to factory method

--- src/infra/http/axios/axios-http-client.spec.ts
+++ src/infra/http/axios/axios-http-client.spec.ts
@@ -1,5 +1,6 @@
 import type axios from 'axios';
 
+import { type HttpRequest } from '@/data/protocols/http';
 import { mockHttpRequest } from '@/data/test';
 import { mockAxios, mockHttpResponse } from '@/infra/test/mock-axios';
 
@@ -10,21 +11,27 @@ jest.mock('axios');
 type SutTypes = {
   sut: AxiosHttpClient,
   mockedAxios: jest.Mocked<typeof axios>
+  request: HttpRequest
 };
 
 const makeSut = (): SutTypes => {
   const sut = new AxiosHttpClient();
+  const request = mockHttpRequest();
   const mockedAxios = mockAxios();
   return {
     sut,
-    mockedAxios
+    mockedAxios,
+    request
   };
 };
 
 describe('AxiosHttpClient', () => {
   test('should call axios with correct params', async () => {
-    const { sut, mockedAxios } = makeSut();
-    const request = mockHttpRequest();
+    const {
+      sut,
+      mockedAxios,
+      request
+    } = makeSut();
     await sut.request(request);
     expect(mockedAxios.request).toHaveBeenCalledWith({
       url: request.url,
@@ -35,8 +42,11 @@ describe('AxiosHttpClient', () => {
   });
 
   test('should return correct response', async () => {
-    const { sut, mockedAxios } = makeSut();
-    const request = mockHttpRequest();
+    const {
+      sut,
+      mockedAxios,
+      request
+    } = makeSut();
     const mockResponse = mockHttpResponse();
     mockedAxios.request.mockResolvedValue(mockResponse);
     const response = await sut.request(request);
