276218042f7ca2c96f8d2857e402cf1baacf0a4e
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-05 07:49:43 -0300

    feat: implement UserLocation interface to OpenCageUserLocation

--- src/data/usecases/user-location/open-cage-user-location.ts
+++ src/data/usecases/user-location/open-cage-user-location.ts
@@ -2,7 +2,7 @@ import { HttpStatus, type HttpClient, type HttpResponse } from '@/data/protocols
 import { UnexpectedError } from '@/domain/errors/http';
 import { type UserLocation } from '@/domain/usecases/user-location';
 
-export class OpenCageUserLocation {
+export class OpenCageUserLocation implements UserLocation {
   constructor(
     private readonly httpClient: HttpClient<any>,
     private readonly url: string,
