5ed148fc300973fe4f0891ff48be63e84e850488
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-06 12:28:02 -0300

    feat: remove params from UserLocation get method

--- src/domain/usecases/user-location.ts
+++ src/domain/usecases/user-location.ts
@@ -1,10 +1,8 @@
 import { type UserLocationModel } from '@/data/models';
 import { type HttpResponse } from '@/data/protocols/http';
 
-import { type UserCoordinates } from './user-coordinates';
-
 export interface UserLocation {
-  get: (params: UserCoordinates.Model) => Promise<HttpResponse<UserLocation.Model>>
+  get: () => Promise<HttpResponse<UserLocation.Model>>
 }
 
 export namespace UserLocation {
