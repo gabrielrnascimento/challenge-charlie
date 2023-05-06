e925f23aa8af9d787e05d6748673287eb7c56a2e
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-06 12:39:05 -0300

    feat: add OpenCageParams type

--- src/data/models/user-location-model.ts
+++ src/data/models/user-location-model.ts
@@ -1,5 +1,12 @@
+import { type UserCoordinates } from '@/domain/usecases/user-coordinates';
+
 export type UserLocationModel = {
   city: string,
   state: string,
   country: string
+};
+
+export type OpenCageParams = {
+  key: string,
+  coords: UserCoordinates.Model
 };
