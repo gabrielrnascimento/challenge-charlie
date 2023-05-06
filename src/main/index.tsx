6ccab6d99f2b7e842e44499b3a6a6f1e3bb97b79
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-06 15:57:46 -0300

    chore: move entry file to main folder

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const rootElement = document.getElementById('root') as HTMLElement;
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.tsx',
+  entry: './src/main/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle-[hash].js',
