7df7307170030bcd5e3f806e24550aafea443630
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-07 13:05:12 -0300

    chore: update webpack config with source-map-loader

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: [ 'source-map-loader' ]
       }
     ]
   },
