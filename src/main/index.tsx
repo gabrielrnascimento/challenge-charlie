cede49ee2366e0e2f042d000f9489ced8f75211f
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-07 11:32:07 -0300

    feat: add App component

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { App } from './app';
+
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <h1>Hello, World!</h1>
+  <App/>
 );
