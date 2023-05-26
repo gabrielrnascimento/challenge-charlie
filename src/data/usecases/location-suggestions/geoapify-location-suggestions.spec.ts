7b1a11818060ed01e3b0682d027eb0bd73710de2
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-05-25 22:09:54 -0300

    test: ensure GeoapifyLocationSuggestions throws UnexpectedError on 400

--- src/data/usecases/location-suggestions/geoapify-location-suggestions.spec.ts
+++ src/data/usecases/location-suggestions/geoapify-location-suggestions.spec.ts
@@ -1,27 +1,36 @@
 import { faker } from '@faker-js/faker';
 
+import { HttpStatus } from '@/data/protocols/http';
 import { HttpClientSpy } from '@/data/test';
+import { UnexpectedError } from '@/domain/errors';
 
 import { GeoapifyLocationSuggestions } from './geoapify-location-suggestions';
 
 type SutTypes = {
   sut: GeoapifyLocationSuggestions,
-  httpClientSpy: HttpClientSpy
+  httpClientSpy: HttpClientSpy,
+  searchTerm: string
 };
 
 const makeSut = (): SutTypes => {
   const httpClientSpy = new HttpClientSpy();
   const sut = new GeoapifyLocationSuggestions(httpClientSpy);
+  const searchTerm = faker.address.cityName();
   return {
     sut,
-    httpClientSpy
+    httpClientSpy,
+    searchTerm
   };
 };
 
 describe('GeoapifyLocationSuggestions', () => {
   test('should call HttpClient with correct data', () => {
-    const { sut, httpClientSpy } = makeSut();
-    const searchTerm = faker.address.cityName();
+    const {
+      sut,
+      httpClientSpy,
+      searchTerm
+    } = makeSut();
+
     sut.load(searchTerm);
 
     const {
@@ -33,5 +42,17 @@ describe('GeoapifyLocationSuggestions', () => {
     expect(method).toBe('get');
     expect(url).toBe(sut.url);
     expect(params).toBe(sut.params);
+  });
+
+  test('should throw UnexpectedError on 400', async () => {
+    const {
+      sut,
+      httpClientSpy,
+      searchTerm
+    } = makeSut();
+
+    httpClientSpy.response.statusCode = HttpStatus.badRequest;
+    const response = sut.load(searchTerm);
+    await expect(response).rejects.toThrow(new UnexpectedError());
   });
 });
